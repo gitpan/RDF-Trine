cabbed7c0fdbce5d15169b10dc1bca1620eb431a
Gregory Todd Williams <gwilliams@cpan.org>
2014-05-17 21:58:35 +0000

    Import of GWILLIAMS/RDF-Trine-1.007_02 from CPAN.
    gitpan-cpan-distribution: RDF-Trine
    gitpan-cpan-version:      1.007_02
    gitpan-cpan-path:         GWILLIAMS/RDF-Trine-1.007_02.tar.gz
    gitpan-cpan-author:       GWILLIAMS
    gitpan-cpan-maturity:     developer

--- lib/RDF/Trine/Model.pm
+++ lib/RDF/Trine/Model.pm
@@ -7,7 +7,7 @@ RDF::Trine::Model - Model class
 
 =head1 VERSION
 
-This document describes RDF::Trine::Model version 1.007_01
+This document describes RDF::Trine::Model version 1.007_02
 
 =head1 METHODS
 
@@ -23,7 +23,7 @@ no warnings 'redefine';
 
 our ($VERSION);
 BEGIN {
-	$VERSION	= '1.007_01';
+	$VERSION	= '1.007_02';
 }
 
 use Scalar::Util qw(blessed refaddr);
@@ -248,13 +248,16 @@ sub add_hashref {
 
 =item C<< add_iterator ( $iter ) >>
 
-Add triples from the statement iteratorto the model.
+Add triples from the statement iterator to the model.
 
 =cut
 
 sub add_iterator {
 	my $self	= shift;
 	my $iter	= shift;
+	unless (blessed($iter) and ($iter->is_graph)) {
+		throw RDF::Trine::Error::MethodInvocationError -text => 'Cannot add a '. ref($iter) . ' iterator to a model, only graphs.';
+	}
 	$self->begin_bulk_ops();
 	while (my $st = $iter->next) {
 		$self->add_statement( $st );
